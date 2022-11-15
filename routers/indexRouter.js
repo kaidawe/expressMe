c11c532c1c999e21efd83d8a67a81c3f4ed48718
Haley Dawe <hdawe@my.bcit.ca>
2022-11-15 12:59:31 -0800

    updated the contact form

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -10,4 +10,21 @@ indexRouter.get("/", (req, res) =>{
 indexRouter.get("/about", (req, res) => {
   res.render("about", indexTitle);
 });
+
+
+indexRouter.get("/contact", (req, res) => {
+  res.render("contact", {
+    title: "Express Yourself - Contact Us",
+    status: null,
+  });
+});
+
+indexRouter.post("/contact", (req, res) => {
+  res.render("contact", {
+    title: "Express Yourself - Contact Us",
+    status: "received",
+    formData: req.body,
+  });
+});
+
 module.exports = indexRouter;
