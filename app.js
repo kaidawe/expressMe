5383e7a0d61eba93df6cbb53087cd91bb21f27e4
Haley Dawe <hdawe@my.bcit.ca>
2022-11-15 13:10:08 -0800

    added the api router information

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const PORT = process.env.PORT || 3004;
 
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 // load our routers
 const indexRouter = require("./routers/indexRouter");
 const apiRouter = require("./routers/apiRouter");
 const profilesRouter = require("./routers/profilesRouter");
+const apiRouter = require("./routers/apiRouter");
 
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(cors({ origin: [/127.0.0.1*/, /localhost*/] }));
 
 const logger = require("morgan");
 app.use(logger("dev")); 
@@ -25,12 +30,17 @@ app.set("layout", "./layouts/full-width");
 
 
 
-app.get("/yuko", (req, res) => res.sendFile(__dirname + "/pages/yuko"));
-app.get("/haley", (req, res) => res.sendFile(__dirname + "/pages/haley"));
-app.get("/sam", (req, res) => res.sendFile(__dirname + "/pages/sam"));
+// app.get("/yuko", (req, res) => res.sendFile(__dirname + "/pages/yuko"));
+// app.get("/haley", (req, res) => res.sendFile(__dirname + "/pages/haley"));
+// app.get("/sam", (req, res) => res.sendFile(__dirname + "/pages/sam"));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
 app.use(indexRouter);
+
+app.use("/api", apiRouter);
 
 app.use("/profiles", profilesRouter);
 
